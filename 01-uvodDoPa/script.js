e276d0b3dded527b125a1de8ac8551a1ff583f6c
Stefan Rajilić <44003786+stefan-rajilic@users.noreply.github.com>
2024-09-15 11:18:27 +0200

    První úvod do VSCODE, JS a HTML

---
+++ 01-uvodDoPa/script.js
@@ -0,0 +1,16 @@
+function ahoj() {
+    let x = 1+1
+    console.log("AHOJ VŠICHNI!");
+    const y = x + x*2
+    console.log(x) // 2 
+    console.log(y) // 6
+    console.log(x, " A TEĎ BUDE Y: " ,y);
+}
+
+ahoj()
+
+// EXTENSIONS FOR DOWNLOAD:
+// LIVE SERVER - spuštění aplikace v prohlížeči
+// MATERIAL ICON - jiné ikonky => lepší přehled
+// PRETTIER - formátování kódu pomocí klávesové zkratky (lepší přehlednost)
+// JAVASCRIPT code snippets (OPTIONAL - nepotřebujete)
